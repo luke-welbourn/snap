c801dac55efc68d94b61e4fd2f15a4cfdb8f9e22
Luke Welbourn <luke.tarquin5@outlook.com>
2023-09-27 14:36:07 +0100

    snap game currently uses a timer element.

--- src/main/java/org/example/Snap.java
+++ src/main/java/org/example/Snap.java
@@ -1,18 +1,59 @@
 package org.example;
 
+import javax.management.timer.Timer;
 import java.util.Scanner;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
 
 public class Snap extends CardGame {
+    private Player player1 = new Player("");
+    private Player player2 = new Player("");
+    private Player currentPlayer;
     private Scanner scanner;
 
+    private long timer;
+
+    private String snapInput = "";
+    private String beforeSnapInput = "";
+
+
     public Snap() {
+        currentPlayer = player1; // Player 1 starts
         scanner = new Scanner(System.in);
     }
 
+    private boolean fiveSeconds() {
+
+        long currentTime = System.currentTimeMillis();
+
+        long timeElapsed = currentTime - timer;
+
+        return timeElapsed >= 5000;
+    }
+
+
     public void playSnap() {
+
         shuffleDeck();
-        System.out.println("\nWelcome to Solo Snap! Press Enter to start the game.");
+        System.out.println("\nWelcome to Snap! Press Enter to start the game.");
         scanner.nextLine();
+        System.out.println("\nPlease enter the name of player one:");
+        String playerOneName = scanner.nextLine();
+        while (playerOneName.isEmpty()) {
+            System.out.println("\nName cannot be empty");
+            System.out.print("\nPlease enter the name of player one:");
+            playerOneName = scanner.nextLine();
+        }
+        player1.setName(playerOneName);
+        System.out.println("\nPlease enter the name of player two:");
+        String playerTwoName = scanner.nextLine();
+        while (playerTwoName.isEmpty()) {
+            System.out.println("\nName cannot be empty");
+            System.out.print("\nPlease enter the name of player two:");
+            playerTwoName = scanner.nextLine();
+        }
+        player2.setName(playerTwoName);
+
 
         Card previousCard = null;
 
@@ -26,16 +67,59 @@ public class Snap extends CardGame {
 
             System.out.println("\nCurrent Card: " + currentCard.getStringValue());
 
-            if (previousCard != null && currentCard.getIntegerValue() == previousCard.getIntegerValue()) {
-                System.out.println("\nSnap! You win!");
+
+
+
+
+            // current problem child
+
+            if (previousCard != null && currentCard.getIntegerValue() == previousCard.getIntegerValue() && !snapInput.equals("snap")) {
+
+                timer = System.currentTimeMillis();
+
+                while (!fiveSeconds()){
+                    System.out.println("what is it?");
+                    beforeSnapInput = scanner.nextLine();
+                    if (beforeSnapInput.equals("snap")) {
+                        break;
+                    } else if (fiveSeconds()) {
+                        break;
+                    }
+                }
+
+                if (!fiveSeconds()) {
+                    snapInput = beforeSnapInput;
+                } else {
+                    System.out.println("gotta be quicker");
+                }
+
+
+            }
+
+
+            if (previousCard != null && currentCard.getIntegerValue() == previousCard.getIntegerValue() && snapInput.equals("snap")) {
+                currentPlayer = (currentPlayer == player1) ? player2 : player1;
+                System.out.println("\nSnap! " + currentPlayer.getName() + " wins!");
+                currentPlayer.incrementScore();
                 break;
             }
 
-            previousCard = currentCard;
-            System.out.println("\nPress Enter for the next card...");
+            System.out.println("\n" + currentPlayer.getName() + ", press Enter for the next card...");
             scanner.nextLine();
+
+
+            previousCard = currentCard;
+
+
+            // Switch to the other player's turn
+            currentPlayer = (currentPlayer == player1) ? player2 : player1;
         }
 
+        // show the scores (obviously)
+        System.out.println("Game Over!");
+        System.out.println(player1.getName() + "'s Score: " + player1.getScore());
+        System.out.println(player2.getName() + "'s Score: " + player2.getScore());
     }
+
 
 }
